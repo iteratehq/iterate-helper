4e57f241420a2958f8107e738bd125ab4bde8e35
Mike Singleton <mike@pickaxe.nyc>
2020-03-19 10:33:10 -0400

    WIP

--- src/helper.js
+++ src/helper.js
@@ -1,1 +1,15 @@
-console.log("hello");
+(function() {
+  console.log(document);
+  const iframe = window.document.getElementById("iterate-iframe");
+  console.log(iframe);
+  if (iframe == null) {
+    return;
+  }
+
+  const iframeWindow = iframe.contentWindow;
+  if (iframeWindow == null) {
+    return;
+  }
+
+  //const window = console.log("hey");
+})();
