85a547c5e497adf511e84ee8ca0800629c0b858d
Mike Singleton <mike@pickaxe.nyc>
2020-03-20 14:33:41 -0400

    Forward commands into the iframe

--- src/helper.js
+++ src/helper.js
@@ -1,6 +1,17 @@
 (function() {
   var iframeWindow;
 
+  // Set the Iterate global object to a queue that collects commands until the
+  // iframe loads and the commands are forwarded into it.
+  var IterateCommandQueue = function() {
+    IterateCommandQueue.appendCommand(arguments);
+  };
+  IterateCommandQueue.queue = [];
+  IterateCommandQueue.appendCommand = function(args) {
+    IterateCommandQueue.queue.push(args);
+  };
+  window.Iterate = IterateCommandQueue;
+
   function receiveMessage(event) {
     // TODO: uncomment this when we deploy the script
     // if (event.origin !== 'https://iteratehq.com') {
@@ -20,6 +31,19 @@
     }
   }
 
+  // Forward an Iterate(...) command into the iframe to be executed
+  function sendCommand(args) {
+    // TODO: Set the targetOrigin from * to the URL of the iframe and determine how to make this work in dev
+    var args = Array.prototype.slice.call(args);
+    iframeWindow.postMessage(
+      {
+        type: 'COMMAND',
+        arguments: args,
+      },
+      '*'
+    );
+  }
+
   function init() {
     var iframe = window.document.getElementById('iterate-iframe');
     if (iframe == null) {
@@ -32,6 +56,15 @@
     }
 
     window.addEventListener('message', receiveMessage, false);
+
+    // Forward any pending commands from the queue into the iframe and replace the queue
+    // with a function that forwards commands directly into the frame
+    window.Iterate.queue.forEach(function(command) {
+      sendCommand(command);
+    });
+    window.Iterate = function() {
+      sendCommand(arguments);
+    };
   }
 
   // Wait until the page has loaded before initializing, to ensure that the iframe is on the page
